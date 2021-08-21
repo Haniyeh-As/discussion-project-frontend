3f86c5f2b98beb8349c862b3def0eba71b198a58
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-21 21:14:17 +0430

    Home Page Views

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row
+        justify="center"
+        content="center"
+    >
+      <v-col
+          cols="12"
+          md="6"
+      >
+
+        <v-card>
+          <v-card-title>
+            <a href="#" class="text-decoration-none black--text">
+              <h2>Laravel Error</h2>
+            </a>
+          </v-card-title>
+
+          <v-card-text>
+            <v-row>
+              <v-col>
+                <p>Haniyeh Asiliyan</p>
+              </v-col>
+
+              <v-col>
+                <p class="text-right">2021-08-21 20:45</p>
+              </v-col>
+            </v-row>
+            <p>Lorem ipsum</p>
+          </v-card-text>
+
+        </v-card>
+
+      </v-col>
+
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: 'Home',
+}
 </script>
