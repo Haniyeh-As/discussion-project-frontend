664de700f420dfe2af291698eda97d880e887e4b
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-21 16:06:43 +0430

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/register">
+        <v-btn
+            text
+        >
+          <span class="mr-2">Register</span>
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+      </router-link>
+
+      <router-link to="/login">
+        <v-btn
+            text
+        >
+          <span class="mr-2">Login</span>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </router-link>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Axios from "./axios";
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+
+  mounted(){
+    Axios.get('threads').then(res => {
+      console.log(res)
+    });
+  }
+};
+</script>
