9b05b23c0d667cb5f781a15d22d67de38031842f
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-25 20:13:32 +0430

    Dynamic Threads In Home Page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,26 +7,27 @@
       <v-col
           cols="12"
           md="6"
+
+          v-for="(thread, index) in threads" :key="index"
       >
 
         <v-card>
           <v-card-title>
-              <router-link to="/thread/laravel+error" class="text-decoration-none black--text">
-                <h2>Laravel Error</h2>
+              <router-link :to="'/thread/'+thread.slug" class="text-decoration-none black--text">
+                <h2>{{thread.title}}</h2>
               </router-link>
           </v-card-title>
 
           <v-card-text>
             <v-row>
               <v-col>
-                <p>Haniyeh Asiliyan</p>
+                <p>{{ thread.user.name }}</p>
               </v-col>
 
               <v-col>
-                <p class="text-right">2021-08-21 20:45</p>
+                <p class="text-right">{{ thread.created_at }}</p>
               </v-col>
             </v-row>
-            <p>Lorem ipsum</p>
           </v-card-text>
 
         </v-card>
@@ -39,7 +40,22 @@
 </template>
 
 <script>
+import {threadsListRequest} from "../requests/Threads";
+
 export default {
   name: 'Home',
+  data: () => ({
+    threads: []
+  }),
+  mounted() {
+    threadsListRequest.then(res => {
+      this.threads = res.data.data
+    }).catch( err => {
+      console.log(err)
+      if (err.response.statusCode !== 200){
+        alert("Failed To Load Date!");
+      }
+    })
+  }
 }
 </script>
