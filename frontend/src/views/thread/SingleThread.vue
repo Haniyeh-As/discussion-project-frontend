e7870e1865059878cf1aaebea761afadb3c66cd9
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-09-01 14:51:48 +0430

    Added New Reply In Thread Page

--- frontend/src/views/thread/SingleThread.vue
+++ frontend/src/views/thread/SingleThread.vue
@@ -1,26 +1,45 @@
 <template>
-<div>
   <v-container>
     <v-row
-    justify="center"
-    content="center"
+        justify="center"
+        content="center"
     >
       <v-col
-      cols="12"
-      md="8"
+          cols="12"
+          md="8"
       >
         <v-card
-        outlined
+            outlined
         >
           <v-card-title>
-            <h2>{{ thread.title }}</h2>
+            <v-row>
+              <v-col
+                  cols="6"
+                  md="6"
+              >
+                <h2>{{ thread.title }}</h2>
+              </v-col>
+              <v-col
+                  cols="6"
+                  md="6"
+                  v-if="userCanDelete"
+              >
+                <v-btn
+                    color="red"
+                    @click="deleteThread"
+                    class="text-right"
+                >
+                  Delete
+                </v-btn>
+              </v-col>
+            </v-row>
+
           </v-card-title>
           <v-card-text>
             <v-row>
               <v-col>
                 <p>{{ thread.user.name }}</p>
               </v-col>
-
               <v-col>
                 <p class="text-right">{{ thread.created_at }}</p>
               </v-col>
@@ -31,19 +50,21 @@
       </v-col>
 
       <v-col
-      cols="12"
-      md="8"
+          cols="12"
+          md="8"
       >
         <v-row>
-          <v-col><h2>Replies:</h2></v-col>
+          <v-col><h2>Replies: </h2></v-col>
           <v-col
               class="text-right"
+              v-if="isAuth"
           >
             <v-btn
                 ripple
                 to="#"
                 color="green"
                 dark
+                @click="showReplyBox = !showReplyBox"
             >
               Reply
             </v-btn>
@@ -51,113 +72,114 @@
         </v-row>
       </v-col>
 
-      <v-row
-          justify="center"
-          content="center"
+      <v-col
+          cols="12"
+          md="8"
+          v-if="showReplyBox"
       >
-        <v-col
-            cols="12"
-            md="8"
+        <v-card
+            outlined
         >
-          <v-card
-              outlined
-          >
-            <v-card-title>
-              <v-avatar
-                  color="orange"
-                  size="62"
+          <v-card-title>
+            <v-row class="ml-6">
+              <v-col
+                  cols="6"
+                  md="6"
               >
-                HA
-              </v-avatar>
-              <v-row class="ml-6">
-                <v-col
-                    cols="12"
-                    md="12"
-                >
-                  <h5>Haniyeh Asiliyan</h5>
-                </v-col>
-
-                <v-col
-                    cols="12"
-                    md="12"
-                >
-                  <small class="grey--text" style="font-size: 10pt">2021/08/22 12:55</small>
-                </v-col>
-              </v-row>
-            </v-card-title>
-            <v-card-text>
-
-              <p>Lorem ipsum dolor sit amet, stet ornatus ius ei, mei eu oratio mnesarchum. No vitae dissentiet usu, has no legere nonumes. Ex ridens aeterno nam, eu vim delenit dolorum deserunt. Tation appetere vis ex. An audiam euripidis prodesset per, quo bonorum referrentur eu. Id eos admodum convenire, ei duis feugait euripidis vix. Quo eros soleat ea, noster nusquam disputando mel ut, in possim dignissim nec.
-
-                Ius voluptaria posidonium interesset te, eu ius tation dissentiet, cu iudico nullam vel. Pri nostrud perpetua id, an oportere neglegentur ius, usu deserunt rationibus disputando in. Illum mazim phaedrum ei has. Ut dicit minimum salutatus mea. Lucilius dissentiunt mea an, diam copiosae in usu.
-
-                Vix eu epicurei tacimates, ei pro solum veritus. Eu impetus incorrupte adversarium has, natum verear temporibus in ius, cu mei tale molestie. Bonorum inermis erroribus vix ei. Vel et illud doctus abhorreant, primis ocurreret per an. Minim affert in pro.
-
-                Ne quem deserunt conclusionemque mei. Te sit tale splendide. Ea est debitis conclusionemque, nam inani necessitatibus id. Cu commodo legimus neglegentur vel. Hinc civibus vix te, mentitum adolescens cu quo, eu option accusata sed.
-
-                Munere liberavisse theophrastus cu vim. Eros tacimates indoctum vix ea. Mel graeci nominati moderatius id, probo everti pro te. Id aeque oblique mnesarchum eam, unum utinam prompta cu pri. Tota mundi ridens qui et. Eu nec adhuc dicat dolore.</p>
-            </v-card-text>
-          </v-card>
-
-        </v-col>
-
-        <v-col
-            cols="12"
-            md="8"
-        >
-          <v-card
-              outlined
-          >
-            <v-card-title>
-              <v-avatar
-                  color="orange"
-                  size="62"
+                <h5>Submit New Reply</h5>
+              </v-col>
+              <v-col
+                  cols="6"
+                  md="6"
+                  class="text-right"
               >
-                HA
-              </v-avatar>
-              <v-row class="ml-6">
-                <v-col
-                    cols="12"
-                    md="12"
+                <v-btn
+                    color="red"
+                    dark
+                    @click="showReplyBox = false"
                 >
-                  <h5>Haniyeh Asiliyan</h5>
-                </v-col>
+                  Close
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-textarea
+                  label="Content"
+                  outlined
+                  rows="12"
+                  v-model="replyData.content"
+              ></v-textarea>
+              <v-btn
+                  color="blue"
+                  dark
+                  @click="submitNewReply"
+              >
+                Submit
+              </v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
 
-                <v-col
-                    cols="12"
-                    md="12"
+      <v-col
+          cols="12"
+          md="8"
+          v-if="thread.answers.length > 0"
+      >
+        <v-row>
+          <v-col
+              cols="12"
+              md="12"
+              v-for="(answer, index) in thread.answers" :key="index"
+          >
+            <v-card
+                outlined
+            >
+              <v-card-title>
+                <v-avatar
+                    color="orange"
+                    size="62"
                 >
-                  <small class="grey--text" style="font-size: 10pt">2021/08/22 12:55</small>
-                </v-col>
-              </v-row>
-            </v-card-title>
-            <v-card-text>
-
-              <p>Lorem ipsum dolor sit amet, stet ornatus ius ei, mei eu oratio mnesarchum. No vitae dissentiet usu, has no legere nonumes. Ex ridens aeterno nam, eu vim delenit dolorum deserunt. Tation appetere vis ex. An audiam euripidis prodesset per, quo bonorum referrentur eu. Id eos admodum convenire, ei duis feugait euripidis vix. Quo eros soleat ea, noster nusquam disputando mel ut, in possim dignissim nec.
-
-                Ius voluptaria posidonium interesset te, eu ius tation dissentiet, cu iudico nullam vel. Pri nostrud perpetua id, an oportere neglegentur ius, usu deserunt rationibus disputando in. Illum mazim phaedrum ei has. Ut dicit minimum salutatus mea. Lucilius dissentiunt mea an, diam copiosae in usu.
-
-                Vix eu epicurei tacimates, ei pro solum veritus. Eu impetus incorrupte adversarium has, natum verear temporibus in ius, cu mei tale molestie. Bonorum inermis erroribus vix ei. Vel et illud doctus abhorreant, primis ocurreret per an. Minim affert in pro.
-
-                Ne quem deserunt conclusionemque mei. Te sit tale splendide. Ea est debitis conclusionemque, nam inani necessitatibus id. Cu commodo legimus neglegentur vel. Hinc civibus vix te, mentitum adolescens cu quo, eu option accusata sed.
-
-                Munere liberavisse theophrastus cu vim. Eros tacimates indoctum vix ea. Mel graeci nominati moderatius id, probo everti pro te. Id aeque oblique mnesarchum eam, unum utinam prompta cu pri. Tota mundi ridens qui et. Eu nec adhuc dicat dolore.</p>
-            </v-card-text>
-          </v-card>
-
-        </v-col>
-
-
-      </v-row>
-
+                  HA
+                </v-avatar>
+                <v-row class="ml-6">
+                  <v-col
+                      cols="12"
+                      md="12"
+                  >
+                    <h5>{{ answer.user.name }}</h5>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      md="12"
+                  >
+                    <small class="grey--text" style="font-size: 10pt">{{ answer.created_at }}</small>
+                  </v-col>
+                </v-row>
+              </v-card-title>
+              <v-card-text>
+                {{ answer.content }}
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col
+          cols="12"
+          md="8"
+          v-else
+      >
+        No Replies Found
+      </v-col>
     </v-row>
   </v-container>
-</div>
 </template>
 
 <script>
 import VueMarkdown from 'vue-markdown/src/VueMarkdown'
-import {getSingleThreadRequest} from "../../requests/Threads";
+import {getSingleThreadRequest, submitNewReplyForThreadRequest} from "../../requests/Threads";
 
 export default {
   name: "SingleThread",
@@ -180,12 +202,25 @@ export default {
         id:null,
         name: null
       },
+    },
+    showReplyBox: false,
+    replyData: {
+      content: null,
+      thread_id: null
     }
   }),
   methods: {
     fetchThread(){
       getSingleThreadRequest(this.$route.params.slug).then(res => {
         this.thread = res.data
+      })
+    },
+
+    submitNewReply(){
+      this.replyData.thread_id = this.thread.id;
+      submitNewReplyForThreadRequest(this.replyData).then(res => {
+        this.fetchThread();
+        this.showReplyBox = false;
       })
     }
   },
